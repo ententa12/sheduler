54c6f314d87a4efddc819574db3102eba6820aa0
ententa12 <ententa12@gmail.com>
2019-09-24 18:54:58 +0200

    Add project files.

---
+++ Sheduler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
