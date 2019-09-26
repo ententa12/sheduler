38b5573fa3fdddeb2ea17e0917cc8398534807f6
anigh3 <anigh@vp.pl>
2019-09-26 19:07:03 +0200

    nLog

--- Sheduler/Program.cs
+++ Sheduler/Program.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using NLog;
+using System;
 
 namespace Sheduler
 {
     class Program
     {
         static void Main(string[] args)
-        { 
-            EmailSender.SendEmail();
-            ConfigureService.Configure();
+        {
+            //EmailSender.SendEmail();
+            //ConfigureService.Configure();
+
+            try
+            {
+                int zero = 0;
+                int result = 5 / zero;
+            }
+            catch (DivideByZeroException ex)
+            {
+                // get a Logger object and log exception here using NLog. 
+                // this will use the "fileLogger" logger from our NLog.config file
+                Logger logger = LogManager.GetLogger("fileLogger");
+
+                // add custom message and pass in the exception
+                logger.Error(ex, "Whoops!");
+            }
         }
     }
 }
